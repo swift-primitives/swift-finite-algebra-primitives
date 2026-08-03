86587eb568259d85dd5cd6421ab84c22f2fe6321
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-03 16:45:14 +0200

    Fix import ordering in Test Support (#2)

--- Tests/Support/Finite Algebra Primitives Test Support.swift
+++ Tests/Support/Finite Algebra Primitives Test Support.swift
@@ -3,5 +3,5 @@
 // Test Support spine per [MOD-024]: re-export own product + anchor on the
 // lowest in-scope upstream Test Support (Algebra Primitives Test Support).
 
-@_exported public import Finite_Algebra_Primitives
 @_exported public import Algebra_Primitives_Test_Support
+@_exported public import Finite_Algebra_Primitives
